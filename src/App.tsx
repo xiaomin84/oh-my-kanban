83fb86fc6555843df823ac1da51c8bc4ba77caed
jianlong.chen <9114557@qq.com>
2025-10-19 19:24:42 +0800

    update to use emotion css-in-js to style the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+/** @jsxImportSource @emotion/react */
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { useState } from "react";
+import { css } from "@emotion/react";
+
+const COLUMN_BACKGROUND_COLOR = {
+  todo: "#C9AF97",
+  ongoing: "#FFE799",
+  done: "#C0E8BA",
+};
+
+const kanbanCardStyle = css `
+  margin-bottom: 1rem;
+  padding: 0.6rem 1rem;
+  border: 1px solid gray;
+  border-radius: 1rem;
+  list-style: none;
+  background-color: rgba(255, 255, 255, 0.4);
+  text-align: left;
+  &:hover {
+    box-shadow: 0 0.2rem 0.2rem rgba(0, 0, 0, 0.2),inset 0 1px #fff;
+  }
+`;
+
+const kanbanCardTitleStyle = css `min-height: 3rem;`;
 
 const initialTodoList = [
   { title: "开发任务-1", status: "22-05-22 18:15" },
@@ -24,21 +47,64 @@ const doneList = [
 ];
 
 const KanbanBoard = ({ children }: { children: React.ReactNode }) => (
-  <main className="kanban-board">{children}</main>
+  <main
+    css={css `
+      flex: 10;
+      display: flex;
+      flex-direction: row;
+      gap: 1rem;
+      margin: 0 1rem 1rem;
+    `}
+  >
+    {children}
+  </main>
 );
 
 const KanbanColumn = ({
   children,
-  className,
+  bgColor,
   title,
 }: {
   children: React.ReactNode;
-  className: string;
+  bgColor: string;
   title: React.ReactNode;
 }) => {
-  const combinedClassName = `kanban-column ${className}`;
   return (
-    <section className={combinedClassName}>
+    <section
+      css={css `
+        flex: 1;
+        border: 1px solid gray;
+        border-radius: 1rem;
+        display: flex;
+        flex-direction: column;
+        background-color: ${bgColor};
+
+        & > h2 {
+          margin: 0.6rem 1rem;
+          padding-bottom: 0.6rem;
+          border-bottom: 1px solid gray;
+
+          & > button {
+            float: right;
+            margin-top: 0.2rem;
+            padding: 0.2rem 2rem;
+            border: 0;
+            border-radius: 1rem;
+            height: 1.8rem;
+            line-height: 1rem;
+            font-size: 1rem;
+          }
+        }
+        
+        & > ul {
+          flex: 1;
+          flex-basis: 0;
+          margin: 1rem;
+          padding: 0;
+          overflow: auto;
+        }
+      `}
+    >
       <h2>{title}</h2>
       <ul>{children}</ul>
     </section>
@@ -47,9 +113,9 @@ const KanbanColumn = ({
 
 const KanbanCard = ({ title, status }: { title: string; status: string }) => {
   return (
-    <li className="kanban-card">
-      <div className="card-title">{title}</div>
-      <div className="card-status">{status}</div>
+    <li css={kanbanCardStyle}>
+      <div css={kanbanCardTitleStyle}>{title}</div>
+      <div css={css `text-align: right; font-size: 0.8rem; color: #333;`}>{status}</div>
     </li>
   );
 };
@@ -68,9 +134,18 @@ const KanbanNewCard = ({ onSubmit }: { onSubmit: (title: string) => void }) => {
   };
 
   return (
-    <li className="kanban-card">
+    <li css={kanbanCardStyle}>
       <h3>添加新卡片</h3>
-      <div className="card-title">
+      <div
+        css={[
+          kanbanCardTitleStyle,
+          css`
+            & >input[type="text"] {
+              width: 80%;
+            }
+          `]
+        }
+      >
         <input
           type="text"
           value={title}
@@ -106,7 +181,7 @@ function App() {
         </header>
         <KanbanBoard>
           <KanbanColumn
-            className="column-todo"
+            bgColor={COLUMN_BACKGROUND_COLOR.todo}
             title={
               <>
                 待处理
@@ -121,12 +196,12 @@ function App() {
               <KanbanCard key={index} {...props} />
             ))}
           </KanbanColumn>
-          <KanbanColumn className="column-ongoing" title="进行中">
+          <KanbanColumn bgColor={COLUMN_BACKGROUND_COLOR.ongoing} title="进行中">
             {ongoingList.map((props, index) => (
               <KanbanCard key={index} {...props} />
             ))}
           </KanbanColumn>
-          <KanbanColumn className="column-done" title="已完成">
+          <KanbanColumn bgColor={COLUMN_BACKGROUND_COLOR.done} title="已完成">
             {doneList.map((props, index) => (
               <KanbanCard key={index} {...props} />
             ))}
